eacf2df8dbf5b256e79ce8653171b13fd59ea12b
MacS <macsaluda@gmail.com>
2022-03-16 13:48:45 +0100

    Initial Commit

---
+++ Maciej Card/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Maciej Card
+//
+//  Created by Maciej Sałuda on 16/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Maciej Card/Maciej_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Maciej_CardApp.swift
+//  Maciej Card
+//
+//  Created by Maciej Sałuda on 16/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Maciej_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
